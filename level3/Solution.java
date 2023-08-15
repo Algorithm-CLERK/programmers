cb48ca58b22cbefbb2490c9521751547ac0092d7
이승민 <0283esm@naver.com>
2023-08-15 19:02:40 +0900

    [#1] level3 clear

--- level3/Solution.java
+++ level3/Solution.java
@@ -1,32 +1,47 @@
-import java.util.*;
+// import java.util.*;
 
+// class Solution {
+//     public int solution(String s) {
+//         int answer = 0;
+//         char[] sC = s.toCharArray();
+//         for (int clen = sC.length; clen > 0; clen--) {
+//             for (int i = 0; i <= sC.length - clen; i++) {
+//                 // 원본 char 배열
+//                 char[] c1 = new char[clen];
+//                 for (int k = i; k < i + clen; k++) {
+//                     c1[k - i] = sC[k];
+//                 }
+//                 // 역순 char 배열
+//                 char[] c2 = new char[clen];
+//                 for (int k = i; k < i + clen; k++) {
+//                     c2[k - i] = sC[i + clen - 1 - (k - i)];
+//                 }
+//                 if (String.valueOf(c1).equals(String.valueOf(c2))) {
+//                     return clen;
+//                 }
+//             }
+//         }
+//         return answer;
+//     }
+// }
 class Solution {
     public int solution(String s) {
-        int answer = 0;
 
-        char[] sC = s.toCharArray();
-
-        // 투 포인터
-        for (int i = 0; i < sC.length; i++) {
-            for (int j = sC.length - 1; j >= i; j--) {
-                // i ~ j까지 길이
-                // 원본 char 배열
-                char[] c1 = new char[j - i + 1];
-                for (int k = i; k <= j; k++) {
-                    c1[k - i] = sC[k];
-                }
-
-                // 역순 char 배열
-                char[] c2 = new char[j - i + 1];
-                for (int k = i; k <= j; k++) {
-                    c2[k - i] = sC[j + i - k];
-                }
-                if (String.valueOf(c1).equals(String.valueOf(c2))) {
-                    answer = Math.max(answer, c1.length);
-                }
+        for (int i = s.length(); i > 0; i--) {
+            for (int j = 0; j + i <= s.length(); j++) {
+                if (isPalindrome(s, j, j + i - 1))
+                    return i;
             }
         }
 
-        return answer;
+        return 0;
+    }
+
+    boolean isPalindrome(String value, int start, int end) {
+        while (start <= end) {
+            if (value.charAt(start++) != value.charAt(end--))
+                return false;
+        }
+        return true;
     }
 }
